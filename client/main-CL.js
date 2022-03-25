26cb02a7ce324a803ad493ff8dac169d856e73d4
kayleen <8iscogb9@anonaddy.me>
2022-03-25 12:04:39 -0500

    nothing is really improved

--- client/main-CL.js
+++ client/main-CL.js
@@ -4,7 +4,7 @@ const findComplimentSubmit = document.getElementById('#findComplimentSubmit');
 const editComplimentSubmit = document.getElementById('#editComplimentSubmit');
 
 // inputs
-const newCompliment = document.getElementById('#newCompliment');
+const newComplimentTxt = document.getElementById('#newCompliment');
 const findCompliment = document.getElementById('#findCompliment');
 const editComplimentIndex = document.getElementById('#editComplimentIndex');
 const editCompliment = document.getElementById('#editCompliment');
@@ -12,40 +12,25 @@ const dltComplimentIndex = document.getElementById('#dltComplimentIndex');
 
 // handle submits
 newComplimentSubmit.addEventListener('click', () => {
-    axios
-        .get('http://localhost:4000/api/compliment')
-        .then(res => doThis(res.data))
+    newComplimentTxt.body
+    // axios
+    //     .get('http://localhost:4000/api/sd')
+    //     .then(res => doThis(res.data))
 });
 
-findComplimentSubmit.addEventListener('click', () => {
-    axios
-        .get(`http://localhost:4000/api/inventory/${paramsInput.value}`)
-        .then(res => doThis([res.data]))
-});
+// findComplimentSubmit.addEventListener('click', () => {
+//     // axios
+//     //     .get(`http://localhost:4000/api/complimentw`)
+//     //     .then(res => doThis([res.data]))
+// });
 
-editComplimentSubmit.addEventListener('click', () => {
-    axios
-        .get(`http://localhost:4000/api/inventory?item=${queryInput.value}`)
-        .then(res => doThis(res.data))
-});
+// editComplimentSubmit.addEventListener('click', () => {
+//     // axios
+//     //     .get(`http://localhost:4000/api/complimentz`)
+//     //     .then(res => doThis(res.data))
+// });
 
 // handle response
-function doThis(dataArr) {
-    responseSection.innerHTML = null;
-
-    if (dataArr.length === 0) {
-        const p = document.createElement('p');
-        const t = document.createTextNode("Response came back with no results!");
-        p.appendChild(t);
-
-        responseSection.appendChild(p)
-    } else {
-        dataArr.forEach(item => {
-            const p = document.createElement('p');
-            const t = document.createTextNode(item)
-            p.appendChild(t);
-    
-            responseSection.appendChild(p)
-        })
-    }
-}
+// function doThis(something) {
+//     console.log(res.data)
+// }
